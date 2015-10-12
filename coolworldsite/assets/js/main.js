17153ca3b2f4c349ba3aeb39b3c1c66b13aa2adb
Chris Grimmett <chris@grimtech.net>
2015-10-12 09:49:16 -0700

    initial

---
+++ coolworldsite/assets/js/main.js
@@ -0,0 +1,13 @@
+var opts = {
+    auto : {
+        speed : 3500, 
+        pauseOnHover : false
+    },
+    fullScreen : false, 
+    swipe : true
+};
+
+makeBSS('.slideshow', opts);
+
+
+//console.log('slideshow init')
